9688823deea5cf6b7c0ab439a2d8141ffc4715c9
wyb5221 <1453624811@qq.com>
2025-11-26 18:39:50 +0800

    测试提交

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	task1 "go-myTest01/task"
+	task1 "go-myTest01/task1"
+	task2 "go-myTest01/task2"
 )
 
 func main() {
@@ -10,6 +11,6 @@ func main() {
 	i := 5
 	task1.Test1(i)
 	fmt.Println("调用test1后的值i：", i)
-	task1.Test2(&i)
+	task2.Test2(&i)
 	fmt.Println("调用test2后的值i：", i)
 }
